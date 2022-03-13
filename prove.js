6873d02e56c29aa1d9fc431120f0cbc6ebb87116
juliebekker <95928020+juliebekker@users.noreply.github.com>
2022-03-13 23:49:29 +0100

    ny

--- prove.js
+++ prove.js
@@ -13,10 +13,41 @@ var nav = document.getElementById('nav');
   }
   }
 
-
-
+/*
 document.getElementById("click1").onclick = function() {myFunction()};
 
   function myFunction() {
-    
+
   }
+*/
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
